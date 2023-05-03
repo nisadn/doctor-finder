f25462bb8956952aa831e198befd61a4fd0126d0
Annisa Dian <nugrahani07@gmail.com>
2023-05-03 23:57:07 +0700

    chore: project structure and add chakra

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Flex>
+      <Text bg='black' color='random.100'>Halooo</Text>
+      <Text bg='gray.200' color='green.600'>Halooo</Text>
+    </Flex>
+  );
+}
+
+export default App;
